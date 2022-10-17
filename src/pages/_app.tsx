d60f33c91db9d8dfcc374e48eda4bde3f3b9f321
mariya.saharova <mariya.saharova@simbirsoft.com>
2022-10-17 21:50:18 +0300

    Added the package for faking data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Layout } from "components/Layout";
 import "styles/globals.scss";
-
+// FIXME Переделать db на фейковый на https://fakerjs.dev/guide/usage.html
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
